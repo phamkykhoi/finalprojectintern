ab9d84069225145450f1e51a2a88f806ac910d68
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-02-09 11:36:37 +0700

    validate_with_element_plus

--- resources/js/Pages/TaskGroup/Form.vue
+++ resources/js/Pages/TaskGroup/Form.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script lang="ts" setup>
 import DangerButton from '@/Components/DangerButton.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
@@ -7,7 +7,8 @@ import SecondaryButton from '@/Components/SecondaryButton.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { useForm, usePage, Link } from '@inertiajs/inertia-vue3'
-import { nextTick, ref, defineEmits, watch } from 'vue';
+import { nextTick, ref, defineEmits, watch, reactive } from 'vue';
+import type { FormInstance } from 'element-plus'
 
 const confirmingTaskGroupDeletion = ref(false);
 const nameInput = ref(null);
@@ -23,10 +24,12 @@ const props = defineProps({
     }
 })
 
-const form = useForm({
+const ruleFormRef = ref<FormInstance>()
+
+const taskGroupForm = reactive({
     name: '',
     activityId: props.activityId,
-});
+})
 
 const emit = defineEmits(['closeModal', 'unClose'])
 
@@ -36,27 +39,38 @@ const confirmTaskGroupDeletion = () => {
     confirmingTaskGroupDeletion.value = true;
 };
 
-const saveTaskGroup = () => {
-    form.post(route(''), {
-        preserveScroll: true,
-        onSuccess: () => closeModal(),
-        onError: () => nameInput.value.focus(),
-        onFinish: () => form.reset(),
-    })
+const saveTaskGroup = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+ formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
 };
 
 const closeModal = () => {
     confirmingTaskGroupDeletion.value = false;
     emit('closeModal', false);
-    form.reset();
 };
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
+
+const rules = reactive({
+    name: [{required: true, message: 'Bắt buộc nhập', trigger: 'blur' }],
+})
 
 </script>
 
 <template>
     <section class="space-y-6">
         <Modal :show="isShowModal" @close="closeModal">
-            <form @submit.prevent="saveTaskGroup()" class="space-y-6">
+            <form @submit.prevent="saveTaskGroup(ruleFormRef)" class="space-y-6">
                 <div
                     class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
                     <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalScrollableLabel">
@@ -64,24 +78,31 @@ const closeModal = () => {
                     </h5>
                 </div>
                 <div class="modal-body relative p-4">
-                        <div>
-                            <InputLabel for="name" value="Tên nhóm công việc (bắt buộc)" />
-                            <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" autofocus
-                                autocomplete="name" ref="nameInput" required/>
-                            <InputError class="mt-2" :message="form.errors.name" />
-                        </div>
-                        <div>
-                            <TextInput id="activityId" type="hidden" class="mt-1 block w-full" v-model="form.activityId" disabled />
-                            <InputError class="mt-2" :message="form.errors.activityId" />
-                        </div>
+                    <el-form ref="ruleFormRef" :model="taskGroupForm" status-icon :rules="rules" class="demo-ruleForm">
+                    <el-form-item prop="name">
+                      <InputLabel for="name" value="Task Group Name:" />
+                      <el-input
+                      v-model="taskGroupForm.name"
+                      type="text"
+                      autocomplete="off"
+                      />
+                    </el-form-item>
+                    <el-form-item prop="activityId"  >
+                      <el-input
+                      v-model="taskGroupForm.activityId"
+                      type="hidden"
+                      autocomplete="off"
+                      />
+                    </el-form-item>
+
+                    </el-form>
                 </div>
                 <div
                     class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
                     <SecondaryButton @click="closeModal"> Đóng </SecondaryButton>
-                    <PrimaryButton class="ml-3" @click="saveTaskGroup">Lưu lại</PrimaryButton>
+                    <PrimaryButton class="ml-3" @click="saveTaskGroup(ruleFormRef)">Lưu lại</PrimaryButton>
                 </div>
             </form>
         </Modal>
     </section>
 </template>
-/
