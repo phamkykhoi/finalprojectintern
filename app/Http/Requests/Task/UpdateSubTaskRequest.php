8b13a0c84d1af8eaec528afa674852ca60133ad0
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-02-28 09:58:14 +0700

    name_required

--- app/Http/Requests/Task/UpdateSubTaskRequest.php
+++ app/Http/Requests/Task/UpdateSubTaskRequest.php
@@ -14,6 +14,7 @@ class UpdateSubTaskRequest extends FormRequest
     public function rules()
     {
         return [
+            'name' => ['required'],
             'description' => ['required'],
         ];
     }
