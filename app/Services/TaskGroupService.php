987921899dd94617bab9d0b724638984b5e5ada2
Mazir <nguyenmanh10a7@gmail.com>
2023-03-20 11:43:27 +0700

    fix query count

--- app/Services/TaskGroupService.php
+++ app/Services/TaskGroupService.php
@@ -10,33 +10,49 @@ class TaskGroupService
 {
     public function getInfoTaskGroup($taskGroupId)
     {
-        $countTasking = DB::table('tasks')->where('start_date', '<', Carbon::now())
-            ->whereNotNull('start_date')
-            ->whereNull('end_date')
-            ->whereNull('completed_at')
-            ->where('task_group_id', $taskGroupId)
+        $countTasking = DB::table('tasks')
+            ->where(function($query) use ($taskGroupId){
+                $query->where('start_date', '<=', Carbon::now())
+                ->whereNotNull('start_date')
+                ->whereNull('end_date')
+                ->whereNull('completed_at')
+                ->whereNull('parent_id')
+                ->where('task_group_id', $taskGroupId);
+            })
+            ->orWhere(function($query) use ($taskGroupId){
+                $query->where('end_date', '>=', Carbon::now())
+                ->whereNotNull('start_date')
+                ->whereNotNull('end_date')
+                ->whereNull('completed_at')
+                ->whereNull('parent_id')
+                ->where('task_group_id', $taskGroupId);
+            })
             ->count();
         
         $countOverDate = DB::table('tasks')
-            ->where(function($query) use($taskGroupId){
+            ->where(function($query) use ($taskGroupId){
                 $query->where('end_date', '<', Carbon::now())
                     ->where('task_group_id', $taskGroupId)
+                    ->whereNull('parent_id')
                     ->whereNull('completed_at');
             })
-            ->orWhere(function($query) use($taskGroupId){
-                $query->where('end_date', '<', DB::raw('completed_at'))
+            ->orWhere(function($query) use ($taskGroupId){
+                $query->where('end_date', '<', 'completed_at')
                     ->where('task_group_id', $taskGroupId)
+                    ->whereNull('parent_id')
                     ->whereNotNull('completed_at');
             })
             ->count();
 
         $countTaskFinish = DB::table('tasks')
             ->whereNotNull('completed_at')
+            ->whereNull('parent_id')
             ->where('task_group_id', $taskGroupId)
             ->count();
 
         $countTotalTask = DB::table('tasks')
             ->where('task_group_id', $taskGroupId)
+            ->whereNull('parent_id')
             ->count();
 
         return [
