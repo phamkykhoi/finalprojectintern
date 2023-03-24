94e4911d9aaa62e1fe595f7fa817a079097cda61
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-03-24 09:22:59 +0700

    UI_update

--- resources/js/Pages/Departen/Index.vue
+++ resources/js/Pages/Departen/Index.vue
@@ -2,7 +2,7 @@
 
 import DepartenUserForm from '@/Pages/Departen/User.vue';
 import EditDepartmentForm from '@/Pages/Departen/Edit.vue';
-import { Setting, CirclePlus, Avatar, More, Tools, Star, ChatSquare} from '@element-plus/icons-vue';
+import { Setting, CirclePlus, Avatar, More, Tools, Star, ChatSquare, Edit} from '@element-plus/icons-vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import { Link } from '@inertiajs/inertia-vue3';
 import ActivityForm from '@/Pages/Activity/Form.vue';
@@ -84,7 +84,7 @@ const closeFormDepartmentUser = (value) => {
                                             <a @click="createActivityForm(department)"
                                             class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 transition duration-150 ease-in-out">
                                                 <el-icon class="icon-action"><CirclePlus /></el-icon>
-                                                <i class="bi bi-plus-lg"></i>Tạo hoạt
+                                                <i class="bi bi-plus-lg"></i>Tạo hoạt động
                                             </a>
                                         </div>
                                         <div class="flex" style="align-items: center;">
@@ -107,39 +107,38 @@ const closeFormDepartmentUser = (value) => {
                         </a>
 
                         <div :key="index" v-for="(activity, index) in department.activities" role="acitivities"
-                            x-show="open" class="space-y-2 px-6" aria-label="acitivities">
+                            x-show="open" class="space-y-2 pl-6 " aria-label="acitivities">
+                            <div class="flex activity-link">
                             <Link :href="route('activity.show', {activity: activity.id})"
                                 v-bind:class="{'bg-gray-200': activityId == activity.id}"
-                                class="block p-1 transition-colors duration-200 rounded-md dark:text-light dark:hover:text-light hover:text-gray-700 w-[120%] activity-link">
+                                class="block py-2 pl-3 pr-4 my-1 transition-colors duration-200 rounded-md dark:text-light dark:hover:text-light hover:text-gray-700 w-[120%]">
                                 <!-- {{ activity.name }}  -->
-                                <div class="flex">
-                                    <span class="my-3 truncate w-[90%]" style="font-size:14px">{{ activity.name }}</span>
-                                <Dropdown class="activity-setting duration-100">
+                                    <span class=" truncate w-[90%] block" style="font-size:14px">{{ activity.name }}</span>
+                            </Link>
+                            <Dropdown class="activity-setting duration-100">
                                     <template #trigger>
-                                        <span class="inline-flex rounded-md" @click="(event)=>{event.stopPropagation()}">
-                                            <el-icon class="float-right mt-3.5 " ><Tools /></el-icon>
+                                        <span class="inline-flex rounded-md">
+                                            <el-icon class="float-right mt-3.5 cursor-pointer" ><Tools /></el-icon>
                                         </span>
                                     </template>
                                     <template #content>
                                         <div class="flex" style="align-items: center;">
                                             <a @click="createDepartmentUserForm(department)"
-                                            class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 transition duration-150 ease-in-out">
+                                            class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 transition duration-150 ease-in-out cursor-pointer">
                                                 <el-icon class="icon-action"><Avatar /></el-icon>
                                                 <i class="bi bi-plus-lg"></i>Quản lý thành viên
                                             </a>
                                         </div>
                                         <div class="flex" style="align-items: center;">
                                             <a @click="editDepartmentForm(department)"
-                                            class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 transition duration-150 ease-in-out">
-                                                <el-icon class="icon-action "><Setting /></el-icon>
-                                                <i class="bi bi-plus-lg"></i>Cài đặt
+                                            class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 transition duration-150 ease-in-out cursor-pointer">
+                                                <el-icon class="icon-action "><Edit/></el-icon>
+                                                <i class="bi bi-plus-lg"></i>Chỉnh sửa
                                             </a>
                                         </div>
                                     </template>
                                 </Dropdown>
-                                </div>
-
-                            </Link>
+                        </div>
                         </div>
                     </div>
                 </nav>
@@ -177,6 +176,9 @@ const closeFormDepartmentUser = (value) => {
         opacity: 0;
         transition: opacity 0.1s ease-in-out;
         position: absolute;
-        right: 10px;
+        right: 15px;
+    }
+    .activity-name{
+        width:200px;
     }
 </style>
